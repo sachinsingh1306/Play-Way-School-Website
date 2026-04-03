6391c86ed0350ae0efbe75cc7860c1652b49d15c
SACHIN SINGH <sachin8174056@gmail.com>
2026-04-03 17:01:53 +0530

    fix asset paths

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/Play-Way-School-Website/",
   plugins: [react(), tailwindcss()],
+  base: "/Play-Way-School-Website/",
 });
