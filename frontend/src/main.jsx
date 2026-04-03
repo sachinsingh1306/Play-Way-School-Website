684754f93201eda2d4ff49056efe101a7b145cf3
SACHIN SINGH <sachin8174056@gmail.com>
2026-04-03 17:25:51 +0530

    fix router basename

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Play-Way-School-Website/">
     <AuthProvider>
       <App />
     </AuthProvider>
